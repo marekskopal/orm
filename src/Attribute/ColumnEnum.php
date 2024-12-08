89026bc9482cf25096cfc7301604346c97c6dddf
Marek Skopal <skopal.marek@gmail.com>
2024-12-08 17:03:13 +0100

    Add missing attribute params

--- src/Attribute/ColumnEnum.php
+++ src/Attribute/ColumnEnum.php
@@ -11,8 +11,13 @@ use BackedEnum;
 readonly class ColumnEnum extends Column
 {
     /** @param class-string<BackedEnum>|null $enum */
-    public function __construct(public ?string $enum, public ?string $name = null, public bool $nullable = false,)
+    public function __construct(
+        public ?string $enum,
+        public ?string $name = null,
+        public bool $nullable = false,
+        public mixed $default = null,
+    )
     {
-        parent::__construct(type: 'enum', name: $name, nullable: $nullable);
+        parent::__construct(type: 'enum', name: $name, nullable: $nullable, default: $default);
     }
 }
