2f0034bd35ded5e93bd94dc440f428e640797eff
Marek Skopal <skopal.marek@gmail.com>
2024-12-09 20:57:05 +0100

    Add foreign key

---
+++ src/Attribute/ForeignKey.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MarekSkopal\ORM\Attribute;
+
+use Attribute;
+
+#[Attribute(Attribute::TARGET_PROPERTY)]
+readonly class ForeignKey
+{
+    /** @param class-string<object> $entityClass */
+    public function __construct(public string $entityClass)
+    {
+    }
+}
